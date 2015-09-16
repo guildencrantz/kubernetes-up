712a3dbed6ad296033ca0689689b775795ad53c5
Matt Henkel <matt.henkel@returnpath.com>
2015-09-16 12:48:54 -0600

    Update hyperkube
    Change-Id: Ifca760e8ccc659004deef35b5d28aaa707b14f41

--- kube-up.sh
+++ kube-up.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-set -e
-
 IMG_K8SETCD=gcr.io/google_containers/etcd:2.0.12
 IMG_HYPERKUBE=gcr.io/google_containers/hyperkube:v0.18.2
 IMG_SKYETCD=quay.io/coreos/etcd:v2.0.12
 IMG_KUBE2SKY=gcr.io/google_containers/kube2sky:1.9
 IMG_SKYDNS=gcr.io/google_containers/skydns:2015-03-11-001
 
+docker ps >/dev/null 2>&1
+if [ $? = 1 ]; then
+  echo "Docker commands will be run with sudo"
+  sudo=sudo
+else
+  sudo=""
+fi
+
+set -e
+
+pushd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null
+
 echo "Pulling images..."
 echo
-docker pull $IMG_K8SETCD
+$sudo docker pull $IMG_K8SETCD
 echo
-docker pull $IMG_HYPERKUBE
+$sudo docker pull $IMG_HYPERKUBE
 echo
-docker pull $IMG_SKYETCD
+$sudo docker pull $IMG_SKYETCD
 echo
-docker pull $IMG_KUBE2SKY
+$sudo docker pull $IMG_KUBE2SKY
 echo
-docker pull $IMG_SKYDNS
+$sudo docker pull $IMG_SKYDNS
 
 echo
 echo -n "Starting etcd    "
-docker run --net=host -d \
+$sudo docker run --net=host -d \
   $IMG_K8SETCD \
   /usr/local/bin/etcd \
   --addr=127.0.0.1:4001 \
@@ -31,7 +41,7 @@ docker run --net=host -d \
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Starting k8s     "
-docker run --net=host -d \
+$sudo docker run --net=host -d \
   -v /var/run/docker.sock:/var/run/docker.sock \
   $IMG_HYPERKUBE \
   /hyperkube kubelet \
@@ -46,7 +56,7 @@ docker run --net=host -d \
 echo -e "\e[32mOK\e[39m"
 
 echo -n "Starting proxy   "
-docker run --net=host -d \
+$sudo docker run --net=host -d \
   --privileged \
   $IMG_HYPERKUBE \
   /hyperkube proxy \
@@ -54,29 +64,29 @@ docker run --net=host -d \
   --v=2 >/dev/null
 echo -e "\e[32mOK\e[39m"
 
-echo -n "Waiting for API  "
-while [ 1 ]
-do
-  sleep 1
-  if curl -m1 http://10.0.0.1/api/v1beta3/namespaces/default/pods >/dev/null 2>&1
-  then
-    break
-  fi
-done
-echo -e "\e[32mOK\e[39m"
-
-echo -n "Starting skydns  "
-./kubectl create -f kube-dns.rc.yaml >/dev/null
-./kubectl create -f kube-dns.service.yaml >/dev/null
-echo -e "\e[32mOK\e[39m"
-
-echo -n "Verifying skydns "
-while [ 1 ]
-do
-  sleep 1
-  if nslookup google.com 10.0.0.10 >/dev/null 2>&1
-  then
-    break
-  fi
-done
-echo -e "\e[32mOK\e[39m"
+#echo -n "Waiting for API  "
+#while [ 1 ]
+#do
+#  sleep 1
+#  if curl -m1 http://10.0.0.1/api/v1beta3/namespaces/default/pods >/dev/null 2>&1
+#  then
+#    break
+#  fi
+#done
+#echo -e "\e[32mOK\e[39m"
+#
+#echo -n "Starting skydns  "
+#./kubectl create -f kube-dns.rc.yaml >/dev/null
+#./kubectl create -f kube-dns.service.yaml >/dev/null
+#echo -e "\e[32mOK\e[39m"
+#
+#echo -n "Verifying skydns "
+#while [ 1 ]
+#do
+#  sleep 1
+#  if nslookup google.com 10.0.0.10 >/dev/null 2>&1
+#  then
+#    break
+#  fi
+#done
+#echo -e "\e[32mOK\e[39m"
